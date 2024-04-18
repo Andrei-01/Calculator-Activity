d5e1e758526ea425f5be7587cf18e153d0be1da0
Andrei Baldazo <baldazo.deilandrei@ue.edu.ph>
2024-04-18 15:58:51 +0700

    Secondary change - selection of operation

--- main.py
+++ main.py
@@ -17,3 +17,52 @@ print("")
 input("Welcome to Andrei's Calculator")
 
 subprocess.run("clear", shell=True)
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    return x / y
+ 
+def square(x, y):
+    return x ** y
+   
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+
+print("Operation Selection.")
+print("")
+print("use;")
+print("(+) for Addition")
+print("(-) for Subtraction")
+print("(*)for Multiplication")
+print("(%)for Division")
+print("(^)for Square")
+
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+
+
+input("press enter to continue")
+
+subprocess.run("clear", shell=True)
+
